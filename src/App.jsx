ce190345dbcb4e99c1a20a5ca46df4b224726c34
Gautam Gunecha <gautamgunecha@gmail.com>
2022-08-30 11:56:07 +0530

    reunion-assignment-initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import MyRoutes from "./routes/MyRoutes";
+
+const App = () => {
+  return (
+    <Fragment>
+      <MyRoutes />
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ src/routes/MyRoutes.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import Header from "../components/header/Header";
+
+const MyRoutes = () => {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+};
+
+export default MyRoutes;
